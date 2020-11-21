22753549176ba128f2c09bf72437de8f8ea2f9e0
Wonderland6627 <994015020@qq.com>
2020-11-22 01:20:10 +0800

    驱动面板

---
+++ InterfacePlatformClient/Assets/Scripts/UIPanel/DrivePanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivePanel : UIPanel 
+{
+    public override void InitPanel(object param = null)
+    {
+        base.InitPanel();
+    }
+}
